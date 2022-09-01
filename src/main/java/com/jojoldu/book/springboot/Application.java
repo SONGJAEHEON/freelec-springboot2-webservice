75cc6510d361380c5af2f54526a4a056a9e159ef
2017029834 <2017029834@hanyang.ac.kr>
2022-09-01 19:11:25 +0900

    20220901 19:10 code update

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+// @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
-    // TODO 로그인이나 로그아웃할 때 index.js가 나오는 문제
 }
